6b4b75e2275a019cbf77c1352b32995a07880f07
Damian Tarnawsky <damian@ionic.io>
2022-08-18 12:24:55 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform) {
+    this.init();
+  }
+
+  async init() {
+    await this.platform.ready();
+    console.log('hiding splash');
+    (navigator as any).splashscreen.hide();
+    console.log('splash is hidden');
+  }
 }
